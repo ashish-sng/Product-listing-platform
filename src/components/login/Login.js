e5f5edba6cef11efd22302e4f05e751c0647148a
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-19 11:59:58 +0530

    modified login/signup

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Login.css";
 import LoginForm from "../Forms/LoginForm/LoginForm";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="main__login">
       <div className="main__login__header">
         <h1>Feedback</h1>
         <h3>Add your products and give us your valuable feedback</h3>
       </div>
-      <LoginForm />
+      {/* {signupPopup ? <SignupForm/> : <LoginForm/>} */}
+      <LoginForm redirectSignup={() => {
+        navigate("/signup")
+      }} />
     </div>
   );
 };
--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Signup.css";
 import SignupForm from "../Forms/SignupForm/SignupForm";
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="main__signup">
       <div className="main__signup__header">
         <h1>Feedback</h1>
         <h3>Add your products and give us your valuable feedback</h3>
       </div>
-      <SignupForm />
+      <SignupForm redirectLogin={() => {
+        navigate("/login");
+      } } />
     </div>
   );
 };
